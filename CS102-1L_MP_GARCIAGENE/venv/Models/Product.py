30ab2dcebb461b3d6a50c04e1299ef6bf37233de
gn-grc <genejogarcia.gg@gmail.com>
2020-10-30 14:40:05 +0800

    added product model

---
+++ CS102-1L_MP_GARCIAGENE/venv/Models/Product.py
@@ -0,0 +1,21 @@
+class Product:
+    def __init__(self):
+        self.__productId = None # integer value
+        self.__name = None # string name
+        self.__inStock = None # integer number of product stock
+
+    # getters setters
+    def setProductId(self, id):
+        self.__productId = id
+    def getProductId(self):
+        return self.__productId
+
+    def setName(self, name):
+        self.__name = name
+    def getName(self):
+        return self.__name
+
+    def setStock(self, quantity):
+        self.__inStock = quantity
+    def getStock(self):
+        return self.__inStock
