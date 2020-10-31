47260e7032e87df5a463792b9211ef64a2b83fb0
gn-grc <genejogarcia.gg@gmail.com>
2020-10-31 22:21:10 +0800

    prepared input helper

---
+++ CS102-1L_MP_GARCIAGENE/venv/Helpers/InputHelper.py
@@ -0,0 +1,14 @@
+
+class InputHelper:
+
+    @staticmethod
+    def stringInput(msg):
+        pass
+
+    @staticmethod
+    def integerInput(msg):
+        pass
+
+    @staticmethod
+    def floatInput(msg):
+        pass
