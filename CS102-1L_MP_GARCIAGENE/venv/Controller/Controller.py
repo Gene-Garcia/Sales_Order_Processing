097e9550f5b1a4debaa51cbea67f866bc8ed263c
gn-grc <genejogarcia.gg@gmail.com>
2020-10-31 22:29:02 +0800

    prepared menu display

--- CS102-1L_MP_GARCIAGENE/venv/Controller/Controller.py
+++ CS102-1L_MP_GARCIAGENE/venv/Controller/Controller.py
@@ -2,4 +2,22 @@ class Controller:
 
     def __init__(self):
         print("--Controller")
-        orders = CustomerOrder()
+
+    def showMainMenu(self):
+        """
+        1 Take customer's order
+        2 Create sales order
+        3 Process back orders
+        4 Ship orders
+        5 Bill customer # also insert record in Sales Journal
+        6 Display records
+        """
+        pass
+
+    def showDisplayMenu(self):
+        """
+        1 Customer information
+        2 Sales journal records
+        3 Shipping log
+        """
+        pass
