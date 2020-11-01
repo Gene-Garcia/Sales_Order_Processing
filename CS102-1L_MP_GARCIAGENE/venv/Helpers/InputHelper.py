88bb5fbeb5a0539f2f2385f6fb591485ec141443
gn-grc <genejogarcia.gg@gmail.com>
2020-11-01 13:49:47 +0800

    input helper created

--- CS102-1L_MP_GARCIAGENE/venv/Helpers/InputHelper.py
+++ CS102-1L_MP_GARCIAGENE/venv/Helpers/InputHelper.py
@@ -2,13 +2,34 @@
 class InputHelper:
 
     @staticmethod
-    def stringInput(msg):
-        pass
+    def stringInput(message):
+        userInput = None
+        while True:
+            userInput = input("\n\t" + message + " >>").strip()
+            if (userInput != ""):
+                break
+            print("\tInput empty")
+        return userInput
 
     @staticmethod
-    def integerInput(msg):
-        pass
+    def integerInput(message):
+        userInput = None
+        while True:
+            try:
+                userInput = int(input("\n\t" + message + " >>").strip())
+                break
+            except:
+                print("\tInvalid input, integers only.")
+                continue
+        return userInput
 
     @staticmethod
-    def floatInput(msg):
-        pass
+    def floatInput(message):
+        userInput = None
+        while True:
+            try:
+                userInput = float(input("\n\t" + message + " >>").strip())
+                break
+            except:
+                print("\tInvalid input, decimals only.")
+        return userInput
